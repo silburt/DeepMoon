3e4c8d15453d9603cd91c1364237ac801f737ae6
silburt <silburt@astro.utoronto.ca>
2017-12-16 18:41:39 -0500

    docstrings

--- utils/processing.py
+++ utils/processing.py
@@ -1,20 +1,41 @@
-import numpy as np, pandas as pd, h5py
+import numpy as np
 
-# preprocessing
-def preprocess(Data, dim=256, low=0.1, hi=1):
-    #rescaling and inverting images
-    #https://www.mathworks.com/help/vision/ref/contrastadjustment.html
-    #Since maxpooling is used, we want the interesting stuff (craters) to be 1, not 0.
-    #But ignore null background pixels, keep them at 0.
+# Preprocessing
+def preprocess(Data, dim=256, low=0.1, hi=1.0):
+    """Normalize and rescale (and optionally invert) images.
+
+    Parameters
+    ----------
+    Data : hdf5
+        Data array.
+    dim : integer, optional
+        Dimensions of images, assumes square.
+    low : float, optional
+        Minimum rescale value. Default is 0.1 since background pixels are 0.
+    hi : float, optional
+        Maximum rescale value.
+    """
     for key in Data:
-        Data[key][0] = Data[key][0].reshape(len(Data[key][0]),dim,dim,1)
-        for i,img in enumerate(Data[key][0]):
-            img = img/255.
-            #img[img > 0.] = 1. - img[img > 0.]      #inv color
-            minn, maxx = np.min(img[img>0]), np.max(img[img>0])
-            img[img>0] = low + (img[img>0] - minn)*(hi - low)/(maxx - minn) #linear re-scaling
+        Data[key][0] = Data[key][0].reshape(len(Data[key][0]), dim, dim, 1)
+        for i, img in enumerate(Data[key][0]):
+            img = img / 255.
+            # img[img > 0.] = 1. - img[img > 0.]      #inv color
+            minn, maxx = np.min(img[img > 0]), np.max(img[img > 0])
+            img[img > 0] = low + (img[img > 0] - minn) * (hi - low) / (maxx - minn)
             Data[key][0][i] = img
 
-# used for indexing h5py files
 def get_id(i, zeropad=5):
+    """Properly index hdf5 files.
+
+    Parameters
+    ----------
+    i : int
+        Image number to be indexed.
+    zeropad : integer, optional
+        Number of zeros to pad string.
+
+    Returns
+    -------
+    String of hdf5 index.
+    """
     return 'img_{i:0{zp}d}'.format(i=i, zp=zeropad)
