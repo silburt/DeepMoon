0c15f04384d8e5acf0e715a5ac5d6840d4a4ba9d
silburt <silburt@astro.utoronto.ca>
2018-01-11 14:19:52 -0500

    edit

--- post_processed_craters_plot.py
+++ post_processed_craters_plot.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 """Unique Crater Distribution Functions
-
-Functions for extracting craters from model target predictions and filtering
-out duplicates.
-"""
+    
+    Functions for extracting craters from model target predictions and filtering
+    out duplicates.
+    """
 from __future__ import absolute_import, division, print_function
 
 import numpy as np
@@ -16,6 +16,7 @@ import pandas as pd
 import matplotlib
 matplotlib.use('agg')
 import matplotlib.pyplot as plt
+np.random.seed(42)
 
 #########################
 def get_GT(truth_datatype, minrad=0, maxrad=100):
@@ -56,26 +57,26 @@ def new_crater_check(lo, la, r, GTLong, GTLat, GTRad, thresh_longlat2, thresh_ra
 #########################
 def add_unique_craters(craters, craters_unique, GT, thresh_longlat2, thresh_rad2):
     """Generates unique crater distribution by filtering out duplicates.
-
-    Parameters
-    ----------
-    craters : array
+        
+        Parameters
+        ----------
+        craters : array
         Crater tuples from a single image in the form (long, lat, radius).
-    craters_unique : array
+        craters_unique : array
         Master array of unique crater tuples in the form (long, lat, radius)
-    thresh_longlat2 : float.
+        thresh_longlat2 : float.
         Hyperparameter that controls the minimum squared longitude/latitude
         difference between craters to be considered unique entries.
-    thresh_rad2 : float
+        thresh_rad2 : float
         Hyperparaeter that controls the minimum squared radius difference
         between craters to be considered unique entries.
-
-    Returns
-    -------
-    craters_unique : array
+        
+        Returns
+        -------
+        craters_unique : array
         Modified master array of unique crater tuples with new crater entries.
-    """
-    craters_new_index = []
+        """
+    craters_unique_index = []
     
     km_to_deg = 180. / (np.pi * 1737.4)
     Long, Lat, Rad = craters_unique.T
@@ -93,54 +94,54 @@ def add_unique_craters(craters, craters_unique, GT, thresh_longlat2, thresh_rad2
                 craters_unique = np.vstack((craters_unique, craters[j]))
                 N_match = new_crater_check(lo, la, r, GTLong, GTLat, GTRad, thresh_longlat2, thresh_rad2)
                 if N_match == 0:
-                    craters_new_index.append(j)
+                    craters_unique_index.append(j)
         else:
             craters_unique = np.vstack((craters_unique, craters[j]))
             N_match = new_crater_check(lo, la, r, GTLong, GTLat, GTRad, thresh_longlat2, thresh_rad2)
             if N_match == 0:
-                craters_new_index.append(j)
-    return craters_unique, craters_new_index
+                craters_unique_index.append(j)
+    return craters_unique, craters_unique_index
 
 #########################
 def estimate_longlatdiamkm(dim, llbd, distcoeff, coords):
     """First-order estimation of long/lat, and radius (km) from
-    (Orthographic) x/y position and radius (pix).
-
-    For images transformed from ~6000 pixel crops of the 30,000 pixel
-    LROC-Kaguya DEM, this results in < ~0.4 degree latitude, <~0.2
-    longitude offsets (~2% and ~1% of the image, respectively) and ~2% error in
-    radius. Larger images thus may require an exact inverse transform,
-    depending on the accuracy demanded by the user.
-
-    Parameters
-    ----------
-    dim : tuple or list
+        (Orthographic) x/y position and radius (pix).
+        
+        For images transformed from ~6000 pixel crops of the 30,000 pixel
+        LROC-Kaguya DEM, this results in < ~0.4 degree latitude, <~0.2
+        longitude offsets (~2% and ~1% of the image, respectively) and ~2% error in
+        radius. Larger images thus may require an exact inverse transform,
+        depending on the accuracy demanded by the user.
+        
+        Parameters
+        ----------
+        dim : tuple or list
         (width, height) of input images.
-    llbd : tuple or list
+        llbd : tuple or list
         Long/lat limits (long_min, long_max, lat_min, lat_max) of image.
-    distcoeff : float
+        distcoeff : float
         Ratio between the central heights of the transformed image and original
         image.
-    coords : numpy.ndarray
+        coords : numpy.ndarray
         Array of crater x coordinates, y coordinates, and pixel radii.
-
-    Returns
-    -------
-    craters_longlatdiamkm : numpy.ndarray
+        
+        Returns
+        -------
+        craters_longlatdiamkm : numpy.ndarray
         Array of crater longitude, latitude and radii in km.
-    """
+        """
     # Expand coords.
     long_pix, lat_pix, radii_pix = coords.T
-
+    
     # Determine radius (km).
     km_per_pix = 1. / trf.km2pix(dim[1], llbd[3] - llbd[2], dc=distcoeff)
     radii_km = radii_pix * km_per_pix
-
+    
     # Determine long/lat.
     deg_per_pix = km_per_pix * 180. / (np.pi * 1737.4)
     long_central = 0.5 * (llbd[0] + llbd[1])
     lat_central = 0.5 * (llbd[2] + llbd[3])
-
+    
     # Iterative method for determining latitude.
     lat_deg_firstest = lat_central - deg_per_pix * (lat_pix - dim[1] / 2.)
     latdiff = abs(lat_central - lat_deg_firstest)
@@ -149,88 +150,85 @@ def estimate_longlatdiamkm(dim, llbd, distcoeff, coords):
     lat_deg = lat_central - (deg_per_pix * (lat_pix - dim[1] / 2.) *
                              (np.pi * latdiff / 180.) /
                              np.sin(np.pi * latdiff / 180.))
-    # Determine longitude using determined latitude.
-    long_deg = long_central + (deg_per_pix * (long_pix - dim[0] / 2.) /
-                               np.cos(np.pi * lat_deg / 180.))
-
-    # Return combined long/lat/radius array.
+                             # Determine longitude using determined latitude.
+                             long_deg = long_central + (deg_per_pix * (long_pix - dim[0] / 2.) /
+                                                        np.cos(np.pi * lat_deg / 180.))
+                             
+                             # Return combined long/lat/radius array.
     return np.column_stack((long_deg, lat_deg, radii_km))
 
 
 def extract_unique_craters(CP, craters_unique):
     """Top level function that extracts craters from model predictions,
-    converts craters from pixel to real (degree, km) coordinates, and filters
-    out duplicate detections across images.
-
-    Parameters
-    ----------
-    CP : dict
+        converts craters from pixel to real (degree, km) coordinates, and filters
+        out duplicate detections across images.
+        
+        Parameters
+        ----------
+        CP : dict
         Crater Parameters needed to run the code.
-    craters_unique : array
-        Empty master array of unique crater tuples in the form 
+        craters_unique : array
+        Empty master array of unique crater tuples in the form
         (long, lat, radius).
-
-    Returns
-    -------
-    craters_unique : array
+        
+        Returns
+        -------
+        craters_unique : array
         Filled master array of unique crater tuples.
-    """
-
+        """
+    
     # Load/generate model preds
     preds = h5py.File(CP['dir_preds'], 'r')[CP['datatype']]
     print("Loaded model predictions successfully")
-
+    
     # need for long/lat bounds
     P = h5py.File(CP['dir_data'], 'r')
     llbd, pbd, distcoeff = ('longlat_bounds', 'pix_bounds',
                             'pix_distortion_coefficient')
     dim = (float(CP['dim']), float(CP['dim']))
-    rand = np.random.randint(0, 100, 50)
-
+                            
     # prepare images, detect craters
     imgs = preprocess(P['input_images'][:CP['n_imgs']].astype('float32'))
     GT = get_GT(CP['datatype'])
-
+    rand = np.random.randint(0,CP['n_imgs'],100)
+        
     N_matches_tot = 0
     for i in range(CP['n_imgs']):
         id = proc.get_id(i)
-
+                                    
         coords = tmt.template_match_t(preds[i], minrad=CP['mr'])
-
+                                        
         # convert, add to master dist
         if len(coords) > 0:
-
-            new_craters_unique = estimate_longlatdiamkm(
-                dim, P[llbd][id], P[distcoeff][id][0], coords)
+            
+            new_craters_unique = estimate_longlatdiamkm(dim, P[llbd][id], P[distcoeff][id][0], coords)
             N_matches_tot += len(coords)
-
+                    
             # Only add unique (non-duplicate) craters
             if len(craters_unique) > 0:
-                craters_unique, craters_new_index = add_unique_craters(new_craters_unique, craters_unique, GT, CP['llt2'], CP['rt2'])
-                print(craters_new_index)
-                coords_new = coords[craters_new_index]
+                craters_unique, craters_unique_index = add_unique_craters(new_craters_unique, craters_unique, GT, CP['llt2'], CP['rt2'])
+                coords_unique = coords[craters_unique_index]
             else:
-                craters_new = np.concatenate((craters_unique, new_craters_unique))
-                coords_new = []
-
-        #if len(coords_new) > 0:
-        if i in rand and len(coords_new) > 0:
-            f, (ax1, ax2, ax3) = plt.subplots(1,3, figsize=[25, 10])
-            img = imgs[i].reshape(256,256)
-            ax1.imshow(img, origin='upper', cmap="Greys_r")
-            ax2.imshow(img, origin='upper', cmap="Greys_r")
-
-            x, y, r = coords_new.T
-            for k in range(len(x)):
-                circle = plt.Circle((x[k], y[k]), r[k], color='blue', fill=False, linewidth=2, alpha=0.5)
-                ax2.add_artist(circle)
-            ax3.imshow(preds[i], origin='upper', cmap="Greys_r")
-            fontsize = 30
-            ax1.set_title('Moon Image', fontsize=fontsize)
-            ax2.set_title('%d new CNN-Detected Craters'%len(x), fontsize=fontsize)
-            ax3.set_title('CNN-Pred', fontsize=fontsize)
-            plt.savefig('post_processed_imgs/%d.png'%i)
-            plt.close()
+                craters_unique = np.concatenate((craters_unique, new_craters_unique))
+                coords_unique = coords
+                                            
+            if i in rand:
+                f, (ax1, ax2, ax3) = plt.subplots(1,3, figsize=[25, 10])
+                img = imgs[i].reshape(256,256)
+                ax1.imshow(img, origin='upper', cmap="Greys_r")
+                ax2.imshow(img, origin='upper', cmap="Greys_r")
+                                
+                x, y, r = coords_unique.T
+                for k in range(len(x)):
+                    circle = plt.Circle((x[k], y[k]), r[k], color='blue', fill=False, linewidth=2, alpha=0.5)
+                    ax2.add_artist(circle)
+                ax3.imshow(preds[i], origin='upper', cmap="Greys_r")
+                fontsize = 30
+                ax1.set_title('Moon Image', fontsize=fontsize)
+                ax2.set_title('%d new CNN-Detected Craters'%len(x), fontsize=fontsize)
+                ax3.set_title('CNN-Pred', fontsize=fontsize)
+                plt.savefig('post_processed_imgs/%d.png'%i)
+                plt.close()
 
     np.save(CP['dir_result'], craters_unique)
     return craters_unique
@@ -238,27 +236,27 @@ def extract_unique_craters(CP, craters_unique):
 if __name__ == '__main__':
     # Crater Parameters
     CP = {}
-
+    
     # Image width/height, assuming square images.
     CP['dim'] = 256
-
+    
     # Data type - train, dev, test
     CP['datatype'] = 'test'
-
+    
     # Number of images to extract craters from
-    CP['n_imgs'] = 10000
-
+    CP['n_imgs'] = 100
+    
     # Hyperparameters
     CP['llt2'] = 1.80    # D_{L,L} from Silburt et. al (2017)
     CP['rt2'] = 0.40     # D_{R} from Silburt et. al (2017)
     CP['mr'] = 5
-
+    
     # Location of where hdf5 data images are stored
     CP['dir_data'] = '../moon-craters/datasets/HEAD/%s_images_final.hdf5' % CP['datatype']
-
+    
     # Location of where model predictions are/will be stored
     CP['dir_preds'] = '../moon-craters/datasets/HEAD/HEAD_%spreds_n30000_final.hdf5'%CP['datatype']
-
+    
     # Location of where final unique crater distribution will be stored
     CP['dir_result'] = 'datasets/HEAD/HEAD_%s_craterdist_llt%.2f_rt%.2f_mr%d_fin2.npy' % (CP['datatype'], CP['llt2'], CP['rt2'], CP['mr'])
     
