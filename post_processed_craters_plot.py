455ea4a179c6251a6569027819e23754de8fcbc9
silburt <silburt@astro.utoronto.ca>
2018-01-11 14:31:57 -0500

    working well

--- post_processed_craters_plot.py
+++ post_processed_craters_plot.py
@@ -214,7 +214,7 @@ def extract_unique_craters(CP, craters_unique):
                 genuine_new_craters = []
                                             
             if len(genuine_new_craters) > 0:
-                f, (ax1, ax2, ax3, ax4) = plt.subplots(1,4, figsize=[25, 10])
+                f, (ax1, ax2, ax3, ax4) = plt.subplots(2,2, figsize=[20, 20])
                 img = imgs[i].reshape(256,256)
                 ax1.imshow(img, origin='upper', cmap="Greys_r")
                 ax2.imshow(img, origin='upper', cmap="Greys_r")
@@ -248,7 +248,7 @@ if __name__ == '__main__':
     CP['datatype'] = 'test'
     
     # Number of images to extract craters from
-    CP['n_imgs'] = 100
+    CP['n_imgs'] = 10
     
     # Hyperparameters
     CP['llt2'] = 1.80    # D_{L,L} from Silburt et. al (2017)
