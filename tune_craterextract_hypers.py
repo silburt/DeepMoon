ee41068add7447c979df054b74f9fdf96e732f20
silburt <silburt@astro.utoronto.ca>
2018-01-19 11:24:45 -0500

    edit

--- tune_craterextract_hypers.py
+++ tune_craterextract_hypers.py
@@ -33,21 +33,23 @@ def prep_csvs(craters, nimgs):
 def get_metrics(preds, csvs, nimgs, longlat_thresh2, rad_thresh, template_thresh, target_thresh):
     recall, precision, f1 = [], [], []
     err_lo, err_la, err_r = [], [], []
+    duplicate_match_frac = []
     for i in range(nimgs):
         if len(csvs[i]) < 3:
             continue
-        N_match, N_csv, N_templ, maxr, elo, ela, er, csv_duplicates = template_match_t2c(preds[i], csvs[i],
-                                                                                         longlat_thresh2=longlat_thresh2,
-                                                                                         rad_thresh=rad_thresh,
-                                                                                         template_thresh=template_thresh,
-                                                                                         target_thresh=target_thresh,
-                                                                                         rmv_oor_csvs=0)
+        N_match, N_csv, N_templ, maxr, elo, ela, er, dupe_frac = template_match_t2c(preds[i], csvs[i],
+                                                                                    longlat_thresh2=longlat_thresh2,
+                                                                                    rad_thresh=rad_thresh,
+                                                                                    template_thresh=template_thresh,
+                                                                                    target_thresh=target_thresh,
+                                                                                    rmv_oor_csvs=0)
         if N_match > 0:
-            print(i, N_match, N_csv, N_templ, maxr, csv_duplicates)
+            print(i, N_match, N_csv, N_templ, maxr, dupe_frac)
             p = float(N_match)/float(N_match + (N_templ-N_match))   #assumes unmatched detected circles are FP
             r = float(N_match)/float(N_csv)                         #N_csv = tp + fn, i.e. total ground truth matches
             recall.append(r); precision.append(p); f1.append(2*r*p/(r+p))
             err_lo.append(elo); err_la.append(ela); err_r.append(er)
+            duplicate_match_frac.append(dupe_frac)
         else:
             print("skipping iteration %d,N_csv=%d,N_templ=%d,N_match=%d"%(i,N_csv,N_templ,N_match))
 
@@ -58,6 +60,7 @@ def get_metrics(preds, csvs, nimgs, longlat_thresh2, rad_thresh, template_thresh
     print("mean and std longitude fractional difference between pred and GT craters: %f, %f"%(np.mean(err_lo),np.std(err_lo)))
     print("mean and std latitude fractional difference between pred and GT craters: %f, %f"%(np.mean(err_la),np.std(err_la)))
     print("mean and std radius fractional difference between pred and GT craters: %f, %f"%(np.mean(err_r),np.std(err_r)))
+    print("mean and std duplicate match fraction: %f, %f"%(np.mean(duplicate_match_frac),np.std(duplicate_match_frac)))
 
 if __name__ == '__main__':
     #data parameters
